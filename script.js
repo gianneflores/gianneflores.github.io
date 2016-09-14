277739a2ccf1b34d3de96ab7e257d4546239ab7f
gianneflores <gianneflores18@gmail.com>
2016-09-14 19:36:16 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+});
+
+function cursorAnimation() {
+    $('#cursor').animate({
+        opacity: 0
+    }, 'fast', 'swing').animate({
+        opacity: 1
+    }, 'fast', 'swing');
+}
